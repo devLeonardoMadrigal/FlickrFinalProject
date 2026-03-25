0856089c1c3e861496e06efdf7c674dd57a9aa79
Leonardo Madrigal <DevLeonardoDavid@gmail.com>
2026-03-25 13:37:50 -0400

    Initial Commit

---
+++ FlickrFinalProject/FlickrFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrFinalProjectApp.swift
+//  FlickrFinalProject
+//
+//  Created by Leonardo Madrigal on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FlickrFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
